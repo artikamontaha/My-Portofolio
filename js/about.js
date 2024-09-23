80e9f83afd05f82f788a0d6d668b150fab683247
Najifa Montaha Artika <najifamontahaartika@gmail.com>
2024-09-24 01:57:24 +0600

    I don't know what i add

---
+++ js/about.js
@@ -0,0 +1,3 @@
+document.getElementById('Home_tag').addEventListener('click', function(){
+    window.location.href = './home.html'
+})
---
+++ js/home.js
@@ -0,0 +1,7 @@
+document.getElementById('about_tag').addEventListener('click', function(){
+     window.location.href = './about.html'
+})
+
+document.getElementById('btn_link').addEventListener('click', function(){
+    window.location.href = './about.html'
+})
